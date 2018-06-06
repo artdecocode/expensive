a301d26715757f4f73eddad44089be4d46063fb8
Anton <anton@adc.sh>
2018-06-06 05:06:26 +0100

    build

--- build/index.js
+++ build/index.js
@@ -3,12 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = expensive2;
+exports.default = expensive;
 
 /**
  * This is the main package file.
  */
-async function expensive2() {
-  console.log('expensive2 called');
+async function expensive() {
+  console.log('expensive called');
 }
 //# sourceMappingURL=index.js.map
