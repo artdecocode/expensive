13055d9bdde31580472ab08a4fd6fc04de5262f0
Anton <anton@adc.sh>
2018-06-06 20:59:29 +0100

    build

--- build/bin/index.js
+++ build/bin/index.js
@@ -3,7 +3,11 @@
 /* eslint-disable no-console */
 "use strict";
 
-var _ = require("..");
+var _lib = require("../lib");
+
+var _check = _interopRequireDefault(require("../lib/namecheap/domains/check"));
+
+var _getConfig = _interopRequireDefault(require("../lib/get-config"));
 
 var _erte = require("erte");
 
@@ -15,82 +19,57 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 const LOG = (0, _util.debuglog)('expensive');
 const DEBUG = /expensive/.test(process.env.NODE_DEBUG);
-const [,, _d0, _d1] = process.argv;
-const domain = _d1 ? _d1 : _d0;
+const [,, domain] = process.argv;
 
-const isSingleWord = d => !/\./.test(d);
+if (!domain) {
+  const u = (0, _getUsage.default)();
+  console.log(u);
+  console.log();
+  process.exit(1);
+}
 
-const startupyDomains = ['.co', '.cc', '.io', '.bz', '.app'];
-
-const makeList = d => startupyDomains.map(s => `${d}${s}`); // const usa = us.reduce((acc, length, i) => {
-//   const command = commands[i]
-//   const s = pad(command, i)
-//   return [...acc, s]
-// }, [])
-
-
-const findTaken = (free, total) => {
-  const res = total.filter(t => {
-    const f = free.indexOf(t) < 0;
-    return f;
+const checkSingleWord = async (word, auth) => {
+  const domains = (0, _lib.makeStartupyList)(word);
+  console.log('Checking %s domains: %s', domains.length, domains.join(', '));
+  const res = await (0, _check.default)({ ...auth,
+    domains
   });
-  return res;
+  reportFree(domains, res);
+};
+
+const reportFree = (domains, freeDomains) => {
+  const [free,, total] = domains.reduce(([f, t, tt], dd) => {
+    const isFree = freeDomains.some(d => d == dd);
+    const it = isFree ? (0, _erte.c)(dd, 'green') : (0, _erte.c)(dd, 'red');
+    return [isFree ? [...f, it] : f, isFree ? t : [...t, it], [...tt, it]];
+  }, [[], [], []]);
+  const percent = free.length / total.length * 100;
+  console.log('%s', total.join(', '));
+  console.log('%s% are free', percent);
 };
 
 (async () => {
-  if (!domain) {
-    const u = (0, _getUsage.default)();
-    console.log(u);
-    console.log();
-    process.exit(1);
-  }
-
-  const single = isSingleWord(domain);
-  const domains = single ? makeList(domain) : [];
-  const d = single ? undefined : domain; // const sd = single ? startupyDomains.map(d => `${domain}${d}`).join(', ') : { length: 1 }
-  // const { l } = sd
+  const singleWord = (0, _lib.isSingleWord)(domain);
 
   try {
-    const a = await (0, _.auth)({
+    const auth = await (0, _getConfig.default)({
       global: true
     });
 
-    if (single) {
-      console.log('Checking %s domains: %s', domains.length, domains.join(', '));
-    } else if (domain) {
-      console.log('Checking domain %s', domain);
+    if (singleWord) {
+      await checkSingleWord(domain, auth);
+      return;
     }
 
-    const res = await (0, _.checkDomains)({ ...a,
-      domain: d,
-      domains
+    console.log('Checking domain %s', domain);
+    const res = await (0, _check.default)({ ...auth,
+      domain
     });
 
-    if (single) {
-      let green = 0;
-      let red = 0;
-      domains.forEach(dd => {
-        const s = [];
-        let t;
-
-        if (res.indexOf(dd) >= 0) {
-          t = (0, _erte.c)(dd, 'green');
-          green++;
-        } else {
-          t = (0, _erte.c)(dd, 'red');
-          red++;
-        }
-
-        s.push(t);
-        console.log('%s', s.join(' '));
-      });
-      console.log('%s% are free', green / (green + red) * 100);
+    if (res.length) {
+      console.log('%s is free', (0, _erte.c)(domain, 'green'));
     } else {
-      if (res.length) {
-        console.log('%s is free', (0, _erte.c)(domain, 'green'));
-      } else {
-        console.log('%s is taken', (0, _erte.c)(domain, 'red'));
-      }
+      console.log('%s is taken', (0, _erte.c)(domain, 'red'));
     }
   } catch ({
     stack,
