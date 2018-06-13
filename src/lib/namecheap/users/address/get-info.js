eb8d9d6f87f857da02d17cabb5543a7ebbb0f372
Anton <anton@adc.sh>
2018-06-13 23:31:23 +0100

    get address info doc

--- src/lib/namecheap/users/address/get-info.js
+++ src/lib/namecheap/users/address/get-info.js
@@ -4,11 +4,16 @@ import { extractTag } from '../../..'
 const GET_INFO = 'namecheap.users.address.getInfo'
 
 /**
- * @param {string} domain a domain name to view info for.
+ * Get information about an address id.
+ * @param {Auth} Auth Authentication object.
+ * @param {Config} conf Configuration parameters.
+ * @param {number} conf.id ID of an address.
+ * @returns {Address} An address information.
  */
-const getList = async (Auth = {}, {
-  id,
-}) => {
+const getList = async (Auth = {}, conf) => {
+  const {
+    id,
+  } = conf
   const res = await query({
     ...Auth,
   }, GET_INFO, {
@@ -19,7 +24,31 @@ const getList = async (Auth = {}, {
   return address
 }
 
-const keys = ['FirstName', 'LastName', 'JobTitle', 'Organization', 'Address1', 'Address2', 'City', 'StateProvince', 'StateProvinceChoice', 'Zip', 'Country', 'Phone', 'PhoneExt', 'EmailAddress']
+/**
+ * @typedef {Object} Config
+ * @property {number} id ID of an address.
+ * @typedef {Object} Address
+ * @property {string} FirstName
+ * @property {string} LastName
+ * @property {string} JobTitle
+ * @property {string} Organization
+ * @property {string} Address1
+ * @property {string} Address2
+ * @property {string} City
+ * @property {string} StateProvince
+ * @property {string} StateProvinceChoice
+ * @property {string} Zip
+ * @property {string} Country
+ * @property {string} Phone
+ * @property {string} PhoneExt
+ * @property {string} EmailAddress
+ */
+
+const keys = [
+  'FirstName', 'LastName', 'JobTitle', 'Organization', 'Address1', 'Address2',
+  'City', 'StateProvince', 'StateProvinceChoice', 'Zip', 'Country', 'Phone',
+  'PhoneExt', 'EmailAddress',
+]
 
 const getInfo = (add) => {
   const res = keys.reduce((acc, key) => {
