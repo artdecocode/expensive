484ffbd50304c95a549827b71393c15445237d22
Anton <anton@adc.sh>
2018-06-14 19:10:28 +0100

    simpler example

--- example/simple.js
+++ example/simple.js
@@ -1,15 +1,4 @@
 import Namecheap, { getConfig } from '../src'
-import { debuglog } from 'util'
-
-const LOG = debuglog('expensive')
-const DEBUG = /expensive/.test(process.env.NODE_DEBUG)
-
-const domains = process.argv.slice(3)
-
-if (!domains.length) {
-  console.log('Please enter a domain or domains')
-  process.exit()
-}
 
 (async () => {
   try {
@@ -18,7 +7,9 @@ if (!domains.length) {
     const Auth = await getConfig({ packageName: 'example' })
 
     const nc = new Namecheap(Auth)
-    const check = await nc.domains.check({ domains })
+    const check = await nc.domains.check({
+      domains: ['testt.co', 'testt.cc'],
+    })
     console.log(check)
   } catch ({ stack, message }) {
     DEBUG ? LOG(stack) : console.error(message)
