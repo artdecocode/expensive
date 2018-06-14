b6759307b9900191c9fc997a3ab5945d85727104
Anton <anton@adc.sh>
2018-06-14 11:23:17 +0100

    add blank files

--- src/lib/namecheap/users/address/get-list.js
+++ src/lib/namecheap/users/address/get-list.js
@@ -1,23 +1,41 @@
 import query from '../../../../lib/query'
 import { extractTag } from '../../..'
 
-const GET_LIST = 'namecheap.users.address.getList'
+const COMMAND = 'namecheap.users.address.getList'
 
 /**
- * @param {string} domain a domain name to view info for.
+ * Gets a list of address IDs and address names associated with the user account.
+ * @param {Auth} Auth Authentication object.
+ * @returns {Address[]} An array with addresses.
  */
-const getList = async (Auth = {}) => {
+export default async function getList(Auth = {}) {
   const res = await query({
     ...Auth,
-  }, GET_LIST)
+  }, COMMAND)
   const [{ content: AddressGetListResult }] = extractTag('AddressGetListResult', res)
   const List = extractTag('List', AddressGetListResult)
   const addresses = List.map(({ props }) => props)
-  const { AddressId } = addresses.find(({ IsDefault }) => IsDefault) || {}
-  return {
-    addresses,
-    defaultId: AddressId,
-  }
+  return addresses
 }
 
-export default getList
+/**
+ * Find a default address ID.
+ * @param {Address[]} addresses A list of addresses.
+ * @returns {number} A default address ID.
+ */
+export const findDefault = (addresses) => {
+  const { AddressId } = addresses.find(({ IsDefault }) => IsDefault) || {}
+  return AddressId
+}
+
+/**
+ * @typedef Address
+ * @property {number} AddressID A unique integer value that represents the address profile.
+ * @property {number} AddressName The name of the address profile.
+ * @property {boolean} IsDefault Whether it is a default address.
+ *
+ * @typedef {Object} Auth
+ * @property {string} ApiUser Username required to access the API.
+ * @property {string} ApiKey Password required used to access the API.
+ * @property {string} ClientIp IP address of the client accessing your application (End-user IP address).
+ */
