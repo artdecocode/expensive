aff582e9255b1f36da55bc1799e98a735659f41f
Anton <anton@adc.sh>
2018-06-14 11:23:58 +0100

    add test for address.getList

--- test/context/index.js
+++ test/context/index.js
@@ -1,9 +1,19 @@
+import africa from 'africa'
+
+process.env.SANDBOX = true
+
+const p = africa('expensive-test', {
+  ApiUser: 'api user',
+  ApiKey: 'api key',
+}, { homedir: __dirname })
+
 /**
  * A testing context for the package.
  */
 export default class Context {
   async _init() {
-    console.log('init context')
+    const config = await p
+    this.config = config
   }
   /**
    * Example method.
@@ -11,7 +21,12 @@ export default class Context {
   example() {
     return 'OK'
   }
+  get Auth() {
+    return {
+      ...this.config,
+      ClientIp: '127.0.0.1',
+    }
+  }
   async _destroy() {
-    console.log('destroy context')
   }
 }
