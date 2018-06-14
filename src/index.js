75eced2562f2dc66b8afb9b8a79df5569e194807
Anton <anton@adc.sh>
2018-06-14 19:10:37 +0100

    clear API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// api
-export { default as checkDomains } from './lib/namecheap/domains/check'
-export { default as getInfo } from './lib/namecheap/domains/get-info'
-export { default as getList } from './lib/namecheap/domains/get-list'
-export { default as getAddressList } from './lib/namecheap/users/address/get-list'
-export { default as getAddressInfo } from './lib/namecheap/users/address/get-info'
 export { default as getConfig } from './lib/get-config'
+
+export { default } from './Namecheap'
