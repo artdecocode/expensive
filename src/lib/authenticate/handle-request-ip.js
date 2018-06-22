79d895ebdd388bb87848da99fe974a25e78c1226
Anton <anton@adc.sh>
2018-06-22 11:50:17 +0100

    request automation

--- src/lib/authenticate/handle-request-ip.js
+++ src/lib/authenticate/handle-request-ip.js
@@ -1,32 +1,26 @@
-import authenticate from '.'
-import { launch } from 'chrome-launcher'
 import { askSingle } from 'reloquent'
+import authenticate from '.'
 
-const handleRequestIP = async (message, { phone, user, head }) => {
+const handleRequestIP = async (message, { phone, user }) => {
   const _ip = /Invalid request IP: (.+)/.exec(message)
   if (!_ip) throw new Error('Could not extract IP from the error message')
   const [, ip] = _ip
-  const [password, chrome] = await Promise.all([
-    askSingle({
-      text: `Enter password to white-list ${ip}`,
-    }),
-    launch({
-      chromeFlags: [
-        ...(head ? [] : ['--headless'/*, '--window-size=1000,2000'*/]),
-        // userDataDir,
-        // '--headless', '--disable-gpu',
-      ],
-    }),
-  ])
-
-  const res = await authenticate({
-    user,
-    password,
-    ip,
-    phone,
-    chrome,
+  const password = await askSingle({
+    text: `Enter password to white-list ${ip}`,
   })
-  return res
+
+  try {
+    await authenticate({
+      user,
+      password,
+      ip,
+      phone,
+    })
+    return true
+  } catch ({ message: m, stack }) {
+    console.log(stack)
+    return m
+  }
 }
 
 export default handleRequestIP
