6d358714034d4451e8fd02cf144f19b94760a2d0
Anton <anton@adc.sh>
2019-05-15 21:03:26 +0300

    display hosts w/ @rqt/namecheap upgrade

--- build/bin/commands/info.js
+++ build/bin/commands/info.js
@@ -1,11 +1,23 @@
+let t = require('tablature'); if (t && t.__esModule) t = t.default;
 const printInfo = require('../../lib/print/info');
 
 /**
  * @param {import('@rqt/namecheap')} client
  */
                async function info(client, domain) {
-  const i = await client.domains.getInfo(domain)
+  const [i, hosts] = await Promise.all([
+    client.domains.getInfo(domain),
+    client.dns.getHosts(domain),
+  ])
   printInfo(i)
+  if (hosts.IsUsingOurDNS) {
+    console.log()
+    console.log(t({
+      headings: ['Name', 'Type', 'Address'],
+      data: hosts.hosts,
+      keys: ['Name', 'Type', 'Address'],
+    }))
+  }
 }
 
 module.exports = info
